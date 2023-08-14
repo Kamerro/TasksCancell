756ba94821103bf27639c9d6d95f686af11e3544
Kamil Mikuła <kamimik090@student.polsl.pl>
2023-08-15 00:29:43 +0200

    Dodaj pliki projektów.

---
+++ TasksCancell/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Threading.Thread;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var token = new CancellationTokenSource();
+        Task task = new Task(() => {
+            while (true)
+            {
+            token.Token.ThrowIfCancellationRequested();
+
+                Console.WriteLine("OK \t");
+            }
+        }, token.Token);
+
+        Task.Factory.StartNew(() =>
+        {
+            token.Token.WaitHandle.WaitOne();
+            Console.WriteLine("Stopped");
+
+        });
+
+        task.Start();
+        Console.WriteLine("Task sucessfully started");
+        Console.ReadKey();
+        token.Cancel();
+        
+    }
+}
