3c650a90c74e9353aa5213c92f59c4dd05c9cb1e
Kamil Mikuła <kamimik090@student.polsl.pl>
2023-08-15 00:48:38 +0200

    ok

--- TasksCancell/Program.cs
+++ TasksCancell/Program.cs
@@ -3,23 +3,44 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        var failure = new CancellationTokenSource();
         var token = new CancellationTokenSource();
+        var paranoid = CancellationTokenSource.CreateLinkedTokenSource(failure.Token, token.Token);
+        //Normal token injected:
         Task task = new Task(() => {
             while (true)
             {
-            token.Token.ThrowIfCancellationRequested();
-
+                token.Token.ThrowIfCancellationRequested();
                 Console.WriteLine("OK \t");
             }
         }, token.Token);
 
+        //Paranoid token injected:
+
+        Task task_better = new Task(() => {
+            while (true)
+            {
+                token.Token.ThrowIfCancellationRequested();
+                Console.WriteLine("OK \t");
+            }
+        }, paranoid.Token);
+
+        //Task that waits for normal token cancel:
         Task.Factory.StartNew(() =>
         {
             token.Token.WaitHandle.WaitOne();
-            Console.WriteLine("Stopped");
+            Console.WriteLine("Stopped by normal token");
 
         });
 
+        //Task waits for any of the tokens cancel ("Paranoid"):
+
+        Task.Factory.StartNew(() =>
+        {
+            paranoid.Token.WaitHandle.WaitOne();
+            Console.WriteLine("Stopped by any of the cancelation tokens");
+
+        });
         task.Start();
         Console.WriteLine("Task sucessfully started");
         Console.ReadKey();
