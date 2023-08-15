6f6b2ab59f3cb8d2c06b5dc7ca34572fa61d1172
Kamil Mikuła <kamimik090@student.polsl.pl>
2023-08-15 15:49:36 +0200

    ok

--- TasksCancell/Program.cs
+++ TasksCancell/Program.cs
@@ -12,6 +12,7 @@ internal class Program
             {
                 token.Token.ThrowIfCancellationRequested();
                 Console.WriteLine("OK \t");
+                SpinWait(10000000);
             }
         }, token.Token);
 
@@ -22,6 +23,7 @@ internal class Program
             {
                 paranoid.Token.ThrowIfCancellationRequested();
                 Console.WriteLine("OK \t");
+                SpinWait(10000000);
             }
         }, paranoid.Token);
 
@@ -43,8 +45,32 @@ internal class Program
         });
         task.Start();
         Console.WriteLine("Task sucessfully started");
+
+        //After click any of keys the token will be cancelled:
         Console.ReadKey();
         token.Cancel();
-        
+
+
+        //Wait all additional code with if conditional loop:
+        if(task.Status is not TaskStatus.Canceled && task_better.Status is not TaskStatus.Canceled)
+            Task.WaitAll(new[] { task, task_better }, 5000,token.Token);
+
+        //Or the other way with handling exceptions:
+        try
+        {
+            Task.WaitAll(new[] { task, task_better }, 5000, token.Token);
+
+        }
+        catch (AggregateException ex)
+        {
+            foreach (var exception in ex.InnerExceptions)
+            {
+                Console.WriteLine(exception.Source + "" + exception.Message);
+            }
+        }
+
+
+
     }
+
 }
